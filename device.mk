cc16e341b2dbd412b75f7e238eb450cb3ed729df
Glen Kuhne <kuh@google.com>
2017-04-07 14:54:17 -0700

    fajita: Set default hotspot ssid
    Added a device specific overlay that changes the default name
    from "AndroidAp" to customized name
    
    Test: Builds, manually verified name change
    Bug: 36368950
    Change-Id: I38a5f6fb950d6a45342080bae186a8ed276d94bf

--- device.mk
+++ device.mk
@@ -490,5 +490,9 @@ PRODUCT_BOOT_JARS += \
 # UDFPS
 TARGET_HAS_UDFPS := true
 
+# WiFi
+PRODUCT_PACKAGES += \
+    TargetWifiOverlay
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/oneplus/fajita/fajita-vendor.mk)
